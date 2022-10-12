3b292d4483247d5332cbb59cddccae4d839fca85
HarneetKaur-Kathuria <Harneet.Kathuria@cognologix.com>
2022-10-12 19:58:53 +0530

    ho

--- src/Except1.java
+++ src/Except1.java
@@ -1,5 +1,7 @@
 public class Except1 {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println(45);
+        System.out.println("hello");
     }
 }
